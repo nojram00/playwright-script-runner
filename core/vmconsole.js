8313498a69f06a1378a62ab99e241e2d416f63c9
marjon.godito <marjon.godito@gmanmi.com>
2026-07-21 09:55:29 +0800

    update

--- core/vmconsole.js
+++ core/vmconsole.js
@@ -3,17 +3,17 @@ import EventEmitter from 'node:events';
 class VMConsole extends EventEmitter {
     log(...args) {
         console.log(...args);
-        this.emit('log', ...args);
+        this.emit('log', args.join(''));
     }
 
     info(...args) {
         console.info(...args);
-        this.emit('info', ...args);
+        this.emit('info', args.join(''));
     }
 
     error(...args) {
         console.error(...args);
-        this.emit('error', ...args)
+        this.emit('error', args.join(''))
     }
 }
 
---
+++ deploy-docker.sh
@@ -0,0 +1,1 @@
+docker-compose down && docker-compose up --build -d 2>&1 | tee docker-output.log
